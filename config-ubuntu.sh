58db15e15c321fd50df4820de70025d75d792ecf
Serge Tensen <s.tensen@netboss.nl>
2024-01-30 22:58:09 +0100

    Added 10-sshd-policy.conf

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
+set -e 
+
 dl_url="https://raw.githubusercontent.com/sergetensen/setmeup-scotty/main/"
 dl_files=( authorized_keys tmux.conf 10_sshd_policy.conf )
-
-function check_dl_files() {
-  # $1
-  # $2
-}
 
 mkdir ~/temp
 chmod 700 ~/temp
 cd ~/temp
-curl "$dl_url${dl_files[1]}" -O  
-
-# Update ~/.ssh/authorized_keys
+for dl_file in "${dl_files[@]}"
+do
+#  curl "$dl_url$dl_file" -O
+  echo "$dl_url$dl_file" -O
+done
+cd ..
+rm -rf ~/temp
