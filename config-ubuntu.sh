b7f8ad0660b73d435f6c6d2c55213e73604279a2
Serge Tensen <s.tensen@netboss.nl>
2024-01-31 07:45:20 +0100

    added checks for creation and removal of ~./temp

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -5,13 +5,21 @@ set -e
 dl_url="https://raw.githubusercontent.com/sergetensen/setmeup-scotty/main/"
 dl_files=( authorized_keys tmux.conf 10_sshd_policy.conf )
 
-mkdir ~/temp
-chmod 700 ~/temp
+if [ ! -d "~/temp"]; then
+  mkdir ~/temp
+  chmod 700 ~/temp
+  remove_temp="yes"
+fi
+
 cd ~/temp
+
 for dl_file in "${dl_files[@]}"
 do
   echo "$dl_url$dl_file" -O
   curl "$dl_url$dl_file" -O
 done
-#cd ..
-#rm -rf ~/temp
+
+if $remove_temp="yes"; then
+#  cd ..
+#  rm -rf ~/temp
+fi
