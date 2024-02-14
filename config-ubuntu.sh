931d59ba71e24757d6d1cec2ea74fceb5cd68586
Serge Tensen <s.tensen@netboss.nl>
2024-02-15 00:37:55 +0100

    Thorough changes to update_authorized_keys.sh. Not finished yet

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -4,20 +4,32 @@ set -e
 
 dl_url="https://raw.githubusercontent.com/sergetensen/setmeup-scotty/main/files/"
 dl_files=( authorized_keys tmux.conf 10_sshd_policy.conf )
+0file_md5="d41d8cd98f00b204e9800998ecf8427e"
 
 if [ ! -d ~/temp ]; then
-  mkdir ~/temp
+  mkdir ~/temp && remove_temp="yes"
   chmod 700 ~/temp
-  remove_temp="yes"
 fi
 
 cd ~/temp
 
+# Download the files
 for dl_file in "${dl_files[@]}"
 do
-  echo "$dl_url$dl_file" -O
+  echo "$dl_url$dl_file"
   curl "$dl_url$dl_file" -O
 done
+
+# Update the authorized_keys file
+# If the file does not exist create (copy) it.
+
+#If the files exists but is empty (the default) then add the keys.
+
+authorized_keys_md5=$(md5sum ~/.ssh/authorized_keys | gawk '{ print $1 }')
+if [[ $authorized_keys_md5 == $0file_md5 ]]; then
+  echo "~/.ssh/authorized_keys exists but is empty. Your keys will be added."
+  cat ~/temp/authorized_keys >> ~/.ssh/authorized_keys
+fi
 
 if [[ "$remove_temp" == yes ]]; then
   #cd ..
--- update_authorized_keys.sh
+++ update_authorized_keys.sh
@@ -1,39 +1,64 @@
 #!/bin/bash
 
-# This script will update ~/.ssh/authorized_keys. The starting point is that
-# authentication is hardened by Fido2, hardware-key backed SSH-keys. 
-# ToAdd:
-# Check:
-# - SSHD version
-# - ~/.ssh folder
+# The goal of this script is to add Fido2, hardware backed, ssh public key(s) 
+# to ~/.ssh/authorized_keys of your user. These keys are supported by OpenSSH
+# version 8.2 and up.
 
-# Check version currently installed OpenSSH
-sshd_versions=sshd -V 2>&1 | grep "OpenSSH" | awk '{print $1}'
+# Workflow:
+# - Assume OpenSSH 8.2 or higher is present on the system and notify this.
+# - Is ~/.ssh present?
+#    - No: Create, Continue.
+# - Has ~/.ssh the correct ownership?
+#        - No: Modify, Continue.
+# - Has ~/.ssh a permission of 700?
+#     - No: Modify, Continue.
+# - Download authorized_keys from the git repo to a temp folder.
+# - Is ~/.ssh/authorized_keys present?
+#     - No: Copy downloaded version ti ~/.ssh and finish
+#   - If it is: check if it is empty. If so, append the content of the downloaded version and finish.
+#   - If it is not: check if it has the same content as the downloaded version. If so finisch.
+#   - If not: append content of the downloaded version.
 
+ 
+# Check version currently installed OpenSSH. The method below seems to work
+# on most UNIX-like systems. OpenSSH has the -V option on sshd since 
+#SSHD_MINVER="OpenSSH_8.2"
+#SSHD_ACTVER=$(sshd -V 2>&1 | grep "OpenSSH" | awk '{print $1}')
 
+clear
+echo "This script assumes that OpenSSH 8.2 or up is installed. If not, ^C, update and re-run."
+sleep 1
+echo "Checking ~/.ssh and ~/.ssh/authorized_keys..."
 
-# Define the URL of the authorized keys file
+SSH_DIR="~/.ssh"
+if [ ! -d $SSH_DIR ]; then
+    echo $SSH_DIR" does not exist. Creating it..."
+    sleep 1
+    mkdir $SSH_DIR
+ fi
+chmod 700 $SSH_DIR # Making sure the security of this directory is right.
+
+# Define the variables used for handling the authorized keys file
 URL="http://example.com/authorized_keys"
-
-# Define the path of the existing authorized keys file
 AUTHORIZED_KEYS_FILE="$HOME/.ssh/authorized_keys"
+BACKUP_FILE="$AUTHORIZED_KEYS_FILE.backup"
 
-# Define the path of the backup file
-BACKUP_FILE="$HOME/.ssh/authorized_keys.backup"
+# Download the authorized keys file to a temporary created directory
+TEMP_FILE="$(mktemp)/authorized_keys.tmp"
+curl -s -o "$TEMP_FILE" "$URL"
 
 # Check if the authorized keys file exists and has a size of 0 bytes
 if [ ! -s "$AUTHORIZED_KEYS_FILE" ]; then
     echo "Creating new authorized keys file..."
-    curl -s "$URL" -o "$AUTHORIZED_KEYS_FILE"
+    cp $TEMP_FILE $AUTHORIZED_KEYS_FILE
+    chown $USER:$USER $AUTHORIZED_KEYS_FILE
+    chmod 600 $AUTHORIZED_KEYS_FILE
+    echo "Newly created $AUTHORIZES_KEYS_FILE. Exiting..."
+    exit 0
 elif [ -f "$AUTHORIZED_KEYS_FILE" ]; then
     # Calculate the MD5 hash of the current authorized keys file
     CURRENT_MD5=$(md5sum "$AUTHORIZED_KEYS_FILE" | awk '{print $1}')
-
-    # Download the authorized keys file and calculate its MD5 hash
-    TEMP_FILE=$(mktemp)
-    curl -s "$URL" -o "$TEMP_FILE"
     DOWNLOAD_MD5=$(md5sum "$TEMP_FILE" | awk '{print $1}')
-
     if [ "$DOWNLOAD_MD5" != "$CURRENT_MD5" ]; then
         echo "Updating authorized keys file..."
         cp "$AUTHORIZED_KEYS_FILE" "$BACKUP_FILE"
